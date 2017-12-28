62e84d3b61b7ace2155935abcc983efb55c00905
Tadej Gašparovič <tadejgasparovic@gmail.com>
2017-12-28 22:09:35 +0100

    Fixed fromPubKey; Added fromRedeemScript

--- src/address.js
+++ src/address.js
@@ -2,28 +2,26 @@ var sha256 = require('sha256');
 var RIPEMD160 = require('ripemd160');
 var base58check = require('base58check');
 
-var P2PKH_ADDRESS_VERSION = 0x26;
-var P2SH_ADDRESS_VERSION = 0x05;
+var P2PKH_ADDRESS_VERSION = '26';
+var P2SH_ADDRESS_VERSION = '05';
 
 function fromPubKey(pubkey){
-	var keyHash = hash160(pubkey).writeInt8(P2PKH_ADDRESS_VERSION);
-	var chksum = checksum(keyHash);
+	var keyHash = hash160(pubkey);
+	return base58check.encode(keyHash, P2PKH_ADDRESS_VERSION, 'hex');
+}
 
-	var address = Buffer.concat([keyHash, checksum], keyHash.length + chksum.length);
-
-	return base58check.encode(address);
+function fromRedeemScript(redeemScript){
+	var keyHash = hash160(redeemScript);
+	return base58check,encode(keyHash, P2SH_ADDRESS_VERSION, 'hex');
 }
 
 function hash160(data){
 	return new RIPEMD160().update(Buffer.from(sha256(data, { asBytes: true }))).digest();
 }
 
-function checksum(keyHash){
-	return new Buffer(sha256.x2(keyHash).substring(0, 4), 'hex');
-}
-
 module.exports = {
 
-	fromPubKey: fromPubKey
+	fromPubKey: fromPubKey,
+	fromRedeemScript: fromRedeemScript
 
 };
