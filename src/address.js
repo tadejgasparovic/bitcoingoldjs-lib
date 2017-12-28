09eded3a9d57f6662ce4984393a96d44eca666d3
tady386 <tadej@tady386-tp>
2017-12-28 21:27:09 +0100

    First version

---
+++ src/address.js
@@ -0,0 +1,25 @@
+var sha256 = require('sha256');
+var RIPEMD160 = require('ripemd160');
+var base58check = require('base58check');
+
+var P2PKH_ADDRESS_VERSION = 0x26;
+var P2SH_ADDRESS_VERSION = 0x05;
+
+Address.prototype.fromPubKey = function(pubkey){
+	var keyHash = hash160(pubkey).writeInt8(P2PKH_ADDRESS_VERSION);
+	var chksum = checksum(keyHash);
+
+	var address = Buffer.concat([keyHash, checksum], keyHash.length + chksum.length);
+
+	return base58check.encode(address);
+}
+
+function hash160(data){
+	return new RIPEMD160().update(sha256(data, { asBytes: true })).digest();
+}
+
+function checksum(keyHash){
+	return new Buffer(sha256.x2(keyHash).substring(0, 4), 'hex');
+}
+
+module.exports = Address;
